0b164d75f1cd1a096a8a7619002cac858ec99b71
minhissei <minhissei@github.com>
2021-04-02 09:44:03 +0700

    cmt3

--- mainjs.js
+++ mainjs.js
@@ -44,3 +44,12 @@ function showDivs(n) {
   }
   x[slideIndex - 1].style.display = "block";
 }
+  
+// document.addEventListener('play', function(e){
+//   var audios = document.getElementsByTagName('audio');
+//   for(var i = 0, len = audios.length; i < len;i++){
+//       if(audios[i] != e.target){
+//           audios[i].pause();
+//       }
+//   }
+// }, true);
