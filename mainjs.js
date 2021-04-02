353bddfa27829a5d2ef9bf7cb46a87f2ecac1430
MinhIsSei <minh23102005@gmail.com>
2021-04-02 08:38:11 +0700

    cmt2

--- mainjs.js
+++ mainjs.js
@@ -1,1 +1,46 @@
+$(document).ready(function () {
+  $(window).scroll(function (event) {
+    var pos_body = $("html,body").scrollTop();
+    console.log(pos_body);
+    if (pos_body > 100) {
+      $(".page1").addClass(".back-to-top.hien-ra");
+    } else {
+      $(".page1").removeClass(".back-to-top.hien-ra");
+    }
+    if (pos_body > 550) {
+      $(".back-to-top").addClass("hien-ra");
+    } else {
+      $(".back-to-top").removeClass("hien-ra");
+    }
+  });
+  $(".back-to-top").click(function (event) {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      0
+    );
+  });
+});
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
